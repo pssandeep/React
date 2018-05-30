4f8c279a316f1efed10fbad5c149c55e7545303a
pssandeep <pssandeep@gmail.com>
2018-05-30 23:14:55 +1000

    Add NavBar and more format

--- recipe-props/src/RecipeApp.js
+++ recipe-props/src/RecipeApp.js
@@ -2,30 +2,52 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Recipe from './Recipe';
 import './RecipeApp.css';
+import Navbar from './Navbar';
+
 
 class RecipeApp extends Component {
+
   render() {
+
+    const recipes = [
+      {
+        title : "pasta",
+        ingredients : ["flour", "water"],
+        instructions : "Mix Ingredients",
+        img : "Spaghetti.jpg"      
+      },
+      {
+        title : "Bolognese",
+        ingredients : ["flour", "water"],
+        instructions : "Mix Ingredients",
+        img : "Bolognese.jpg"
+      },
+      {
+        title : "Pizza",
+        ingredients : ["flour", "water"],
+        instructions : "Mix Ingredients",
+        img : "Pizza.jpg"
+      }
+    ];
     return (
       <div className="App">
-        <Recipe
-          title = "pasta"
-          ingredients = {["flour", "water"]}
-          instructions = "Mix Ingredients"
-          img = "Spaghetti.jpg"
-        />
-        <Recipe
-          title = "Bolognese"
-          ingredients = {["flour", "water"]}
-          instructions = "Mix Ingredients"
-          img = "Bolognese.jpg"
-        />
-        <Recipe
-          title = "Pizza"
-          ingredients = {["flour", "water"]}
-          instructions = "Mix Ingredients"
-          img = "Pizza.jpg"
-        />
+        <Navbar />
+        {recipes.map((r,index) => (<Recipe key = {index} title = {r.title} ingredients = {r.ingredients}
+        instructions = {r.instructions} img = {r.img} />))}
       </div>
+        // <Recipe
+        //   title = "Bolognese"
+        //   ingredients = {["flour", "water"]}
+        //   instructions = "Mix Ingredients"
+        //   img = "Bolognese.jpg"
+        // />
+        // <Recipe
+        //   title = "Pizza"
+        //   ingredients = {["flour", "water"]}
+        //   instructions = "Mix Ingredients"
+        //   img = "Pizza.jpg"
+        // />
+      
     );
   }
 }
