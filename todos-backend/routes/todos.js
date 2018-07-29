8ac3fc2ce357b29c64acd2466c4a939dc5d3334b
pssandeep <pssandeep@gmail.com>
2018-07-29 21:12:27 +1000

    Todo - App - Nodejs Backend.

---
+++ todos-backend/routes/todos.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const Todo = require("../models/todo");
+
+router.get("/", function(req, res, next){
+  Todo.find({})
+  .then(todos => {
+    console.log("GET REQUEST");
+    res.send(todos)
+  })
+  .catch(err => next(err));
+});
+
+router.post("/", function(req, res, next){
+  Todo.create(req.body)
+  .then(todos => res.status(201).send(todos))
+  .catch(err => next(err));
+});
+
+router.delete("/:id", function(req, res, next){
+  Todo.findByIdAndRemove(req.params.id)
+  .then(todos => res.send(todos))
+  .catch(err => next(err));
+});
+
+module.exports = router;
